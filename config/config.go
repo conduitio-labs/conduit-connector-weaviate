e029f3ac9098079885e259102ebe38ce50399e2e
Haris Osmanagic <haris@meroxa.io>
2023-08-24 13:15:43 +0200

    change tag

--- config/config.go
+++ config/config.go
@@ -3,6 +3,6 @@ package config
 type Config struct {
 	Endpoint string `json:"endpoint" validate:"required"`
 	Scheme   string `json:"scheme" validate:"required"`
-	ApiKey   string `json:"api_key" validate:omitempty"`
+	ApiKey   string `json:"api_key" validate:"required"`
 	Class    string `json:"class" validate:"required"`
 }
