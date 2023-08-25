80016c9c1342df86e3ab810d9c7e0239f603a673
Haris Osmanagic <haris@meroxa.io>
2023-08-25 19:25:33 +0200

    add more ITs

--- Makefile
+++ Makefile
@@ -5,15 +5,19 @@ MOCKGEN_VERSION		?= v0.2.0
 PARAMGEN_VERSION	?= v0.7.2
 GOLANG_CI_LINT_VER	:= v1.54.2
 
+.PHONY: build
 build:
 	go build -ldflags "-X 'github.com/conduitio-labs/conduit-connector-weaviate.version=${VERSION}'" -o conduit-connector-weaviate cmd/connector/main.go
 
+.PHONY: install-mocken
 install-mocken:
 	go install go.uber.org/mock/mockgen@$(MOCKGEN_VERSION)
 
+.PHONY: test
 test: generate
 	go test $(GOTEST_FLAGS) -race ./...
 
+.PHONY: test-integration
 test-integration:
 	# run required docker containers, execute integration tests, stop containers after tests
 	docker compose -f test/docker-compose.yml up -d
@@ -21,9 +25,11 @@ test-integration:
 		docker compose -f test/docker-compose.yml down; \
 		exit $$ret
 
+.PHONY: generate
 generate: install-mocken install-paramgen
 	go generate ./...
 
+.PHONY: install-paramgen
 install-paramgen:
 	go install github.com/conduitio/conduit-connector-sdk/cmd/paramgen@$(PARAMGEN_VERSION)
 
--- destination/destination_integration_test.go
+++ destination/destination_integration_test.go
@@ -16,14 +16,17 @@ package destination
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"os"
 	"testing"
 	"time"
 
 	sdk "github.com/conduitio/conduit-connector-sdk"
+	"github.com/google/uuid"
 	"github.com/matryer/is"
 	"github.com/weaviate/weaviate-go-client/v4/weaviate"
+	"github.com/weaviate/weaviate-go-client/v4/weaviate/fault"
 )
 
 func TestDestination_Integration_Insert(t *testing.T) {
@@ -61,11 +64,11 @@ func TestDestination_Integration_Insert(t *testing.T) {
 	err = underTest.Open(ctx)
 	is.NoErr(err)
 
-	id := "f9a510b3-5865-40e4-9fe8-e7fbab25b8bc"
+	id := uuid.NewString()
 	want := map[string]any{
 		"product_name": "computer",
 		"price":        220.15,
-		"labels":       []string{"laptop", "navy-blue"},
+		"labels":       []any{"laptop", "navy-blue"},
 		"used":         true,
 	}
 	rec := sdk.Util.Source.NewRecordCreate(
@@ -90,23 +93,164 @@ func TestDestination_Integration_Insert(t *testing.T) {
 	obj := objects[0]
 	got, ok := obj.Properties.(map[string]any)
 	is.True(ok) // expected object properties to be a map[string]any
-	is.Equal(len(want), len(got))
-	for k, v := range want {
-		if k != "labels" {
-			is.Equal(v, got[k])
-			continue
-		}
+	is.Equal(want, got)
+}
 
-		gotLabels, ok := got["labels"].([]any)
-		is.True(ok) // expected `labels` to be a slice
+func TestDestination_Integration_Update(t *testing.T) {
+	integrationTest(t)
 
-		wantLabels, ok := v.([]string)
-		is.True(ok) // expected `labels` to be a slice
+	is := is.New(t)
+	openAIKey := os.Getenv("OPENAI_APIKEY")
+	is.True(openAIKey != "") // expected OPENAI_APIKEY to be set
 
-		for i, l := range wantLabels {
-			is.Equal(l, gotLabels[i])
-		}
+	ctx := context.Background()
+	class := fmt.Sprintf("Products_%v", time.Now().UnixMilli())
+	cfg := map[string]string{
+		"endpoint":           "localhost:18080",
+		"scheme":             "http",
+		"class":              class,
+		"moduleAPIKey.name":  "X-OpenAI-Api-Key",
+		"moduleAPIKey.value": openAIKey,
+		"generateUUID":       "false",
 	}
+
+	client, err := newWeaviateClient(cfg)
+	is.NoErr(err)
+	defer func() {
+		err = client.Schema().
+			ClassDeleter().
+			WithClassName(class).
+			Do(ctx)
+		is.NoErr(err)
+	}()
+
+	underTest := New()
+	err = underTest.Configure(ctx, cfg)
+	is.NoErr(err)
+
+	err = underTest.Open(ctx)
+	is.NoErr(err)
+
+	// Insert record
+	id := uuid.NewString()
+	recInsert := sdk.Util.Source.NewRecordCreate(
+		sdk.Position("test-position"),
+		map[string]string{},
+		sdk.RawData(id),
+		sdk.StructuredData(map[string]any{
+			"product_name": "computer",
+			"price":        220.15,
+			"labels":       []any{"laptop", "navy-blue"},
+			"used":         true,
+		}),
+	)
+
+	n, err := underTest.Write(ctx, []sdk.Record{recInsert})
+	is.NoErr(err)
+	is.Equal(1, n)
+
+	// Update record
+	payloadUpdate := map[string]any{
+		"product_name": "computer",
+		"price":        330.75,
+		"labels":       []any{"laptop", "pink"},
+		"used":         true,
+	}
+	recUpdate := sdk.Util.Source.NewRecordUpdate(
+		sdk.Position("test-position"),
+		map[string]string{},
+		sdk.RawData(id),
+		nil,
+		sdk.StructuredData(payloadUpdate),
+	)
+
+	n, err = underTest.Write(ctx, []sdk.Record{recUpdate})
+	is.NoErr(err)
+	is.Equal(1, n)
+
+	// Verify update
+	objects, err := client.Data().
+		ObjectsGetter().
+		WithClassName(cfg["class"]).
+		WithID(id).
+		Do(ctx)
+	is.NoErr(err)
+	is.Equal(1, len(objects))
+
+	obj := objects[0]
+	got, ok := obj.Properties.(map[string]any)
+	is.True(ok) // expected object properties to be a map[string]any
+	is.Equal(payloadUpdate, got)
+}
+
+func TestDestination_Integration_Delete(t *testing.T) {
+	integrationTest(t)
+
+	is := is.New(t)
+	openAIKey := os.Getenv("OPENAI_APIKEY")
+	is.True(openAIKey != "") // expected OPENAI_APIKEY to be set
+
+	ctx := context.Background()
+	class := fmt.Sprintf("products_%v", time.Now().UnixMilli())
+	cfg := map[string]string{
+		"endpoint":           "localhost:18080",
+		"scheme":             "http",
+		"class":              class,
+		"moduleAPIKey.name":  "X-OpenAI-Api-Key",
+		"moduleAPIKey.value": openAIKey,
+		"generateUUID":       "false",
+	}
+
+	client, err := newWeaviateClient(cfg)
+	is.NoErr(err)
+	defer func() {
+		err = client.Schema().
+			ClassDeleter().
+			WithClassName(class).
+			Do(ctx)
+		is.NoErr(err)
+	}()
+
+	underTest := New()
+	err = underTest.Configure(ctx, cfg)
+	is.NoErr(err)
+
+	err = underTest.Open(ctx)
+	is.NoErr(err)
+
+	// Write record
+	id := uuid.NewString()
+	want := map[string]any{
+		"product_name": "computer",
+		"price":        220.15,
+		"labels":       []any{"laptop", "navy-blue"},
+		"used":         true,
+	}
+	rec := sdk.Util.Source.NewRecordCreate(
+		sdk.Position("test-position"),
+		map[string]string{},
+		sdk.RawData(id),
+		sdk.StructuredData(want),
+	)
+
+	n, err := underTest.Write(ctx, []sdk.Record{rec})
+	is.NoErr(err)
+	is.Equal(1, n)
+
+	recDelete := sdk.Util.Source.NewRecordDelete(nil, nil, sdk.RawData(id))
+	n, err = underTest.Write(ctx, []sdk.Record{recDelete})
+	is.NoErr(err)
+	is.Equal(1, n)
+
+	_, err = client.Data().
+		ObjectsGetter().
+		WithClassName(cfg["class"]).
+		WithID(id).
+		Do(ctx)
+	is.True(err != nil)
+	wErr := &fault.WeaviateClientError{}
+	is.True(errors.As(err, &wErr))
+	is.Equal(404, wErr.StatusCode)
 }
 
 func newWeaviateClient(cfg map[string]string) (*weaviate.Client, error) {
